773c5280c883ed4968fbf28f42ba7f26f2b684b3
小尭 <82511711+XiaoPigYao@users.noreply.github.com>
2021-10-17 10:28:15 +0800

    Update action.py

--- action.py
+++ action.py
@@ -309,9 +309,9 @@ def run_task(info, phone, password):
         print(res_print)
         print(30 * "=")
         # Server 酱推送
+        server_chan_push(info["sc_key"][0],res_print)
         if info["sc_key"]:
             handle_error(server_chan_push, "Server酱", info["sc_key"][0], res_print)
-            server_chan_push(info["sc_key"][0],res_print)
         # Bark 推送
         if info["bark_key"]:
             handle_error(bark_push, "Bark", info["bark_key"][0], 1, res_print)
